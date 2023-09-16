c4d34a67d0634a611fd7e9d9cd92b0f4e13d9826
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2023-09-16 19:49:13 +0700

    add paralel fetch realtime data

--- pararel_test.py
+++ pararel_test.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 from obspy.clients.seedlink import Client as SeedlinkClient
 from obspy.clients.seedlink.easyseedlink import create_client
+import multiprocessing
 import pytz
 import json
 import configparser
@@ -16,7 +17,11 @@ def error_cb(err):
     if err.code() == KafkaError._ALL_BROKERS_DOWN or \
        err.code() == KafkaError._AUTHENTICATION:
         raise KafkaException(err)
-    
+
+# fungsi untuk menampilkan log
+def log_data(data, type="LOG",stat="-"):
+    print(f"[+] <{stat}> <{type}>  {data}")
+
 # Create a ConfigParser instance
 config = configparser.ConfigParser()
 # Load the configuration from the file
@@ -25,7 +30,7 @@ config.read('kafka_config.txt')
 bootstrap_servers = config.get('KafkaConfig', 'bootstrap.servers')
 sasl_username = config.get('KafkaConfig', 'sasl.username')
 sasl_password = config.get('KafkaConfig', 'sasl.password')
-    
+
 # config untuk masuk ke akun confluent 
 kafka_config = {
     'bootstrap.servers': bootstrap_servers, 
@@ -40,18 +45,9 @@ kafka_config = {
 
 # anam topic pada confluent
 kafka_topic = 'WAVE_Station' 
-
-# key partition
-key_partition = "JAGI"
-
+    
 # menggunakan Producer dengan memasukkan kafka_config
 producer = Producer(kafka_config)
-# seedlink untuk mengambil data dari stasiun sensor dengan realtime
-client = SeedlinkClient("geofon.gfz-potsdam.de", 18000)
-
-# fungsi untuk menampilkan log
-def log_data(data, type="LOG"):
-    print(f"[+] <{type}> {data}")
 
 def time_to_seconds(utc_date):
     hour = utc_date.hour
@@ -60,29 +56,6 @@ def time_to_seconds(utc_date):
     total_seconds = hour * 3600 + minute * 60 + second
     return total_seconds
 
-# mengambil data dengan seedLink Client 
-def get_data_seed_link():
-    # Mengambil waktu saat ini
-    utc_time = datetime.now(pytz.UTC)
-    formatted_time = utc_time.strftime('%Y-%m-%dT%H:%M:%S')
-    starttime = UTCDateTime(formatted_time) 
-    endtime = starttime
-    st = client.get_waveforms("GE", key_partition, "*", "BH*", starttime - 60, endtime)
-    value = {}
-
-    for ch in st:
-        value[ch.stats.channel] = ch.data.tolist()
-        log_data(f"Data from {ch.stats.channel} fetch succesfully !!")
-        # interval detik data yang diambil
-        time_interval = int(time_to_seconds(ch.stats.endtime)) - int(time_to_seconds(ch.stats.starttime))
-        log_data(f"from {ch.stats.starttime} to {ch.stats.endtime}")
-        log_data(f"Time interval of the collected data : {time_interval} seconds")
-        log_data(f'Waves Produced lenght : <{len(ch.data)}>')
-
-    # mengubah file dalam bentuk JSON
-    JSON_value = json.dumps(value)
-    return JSON_value
-
 def acked(err, msg):
     """Delivery report callback called (from flush()) on successful or failed delivery of the message."""
     if err is not None:
@@ -90,16 +63,39 @@ def acked(err, msg):
     else:
         print('Produced to: {} [{}] @ {}'.format(msg.topic(), msg.partition(), msg.offset()))
 
+def fetch_data(station_name):
+    # Create a SeedLink client for the station
+    client = SeedlinkClient("geofon.gfz-potsdam.de", 18000)
+    
+    while True:
+        try:
+            # Mengambil waktu saat ini
+            utc_time = datetime.now(pytz.UTC)
+            formatted_time = utc_time.strftime('%Y-%m-%dT%H:%M:%S')
+            starttime = UTCDateTime(formatted_time) 
+            endtime = starttime
+            st = client.get_waveforms("GE", station_name, "*", "BH*", starttime - 60, endtime)
+            value = {}
 
-# fetching data secara terus menerus
-while True:
-    try:
-        print("Producing data...")
-        values = get_data_seed_link()
-        producer.produce(kafka_topic, key=key_partition, value=values,callback=acked)
-        log_data(f'Waves Produced succesfully')
-        # time.sleep(3)
-    except Exception as e:
-        print("No Trace Data\n")
-        print(f"Error: {str(e)}")
-        time.sleep(1)
+            for ch in st:
+                value[ch.stats.channel] = ch.data.tolist()
+                log_data(stat=station_name,data=f"Data from {ch.stats.channel} fetch succesfully !!")
+                # interval detik data yang diambil
+                time_interval = int(time_to_seconds(ch.stats.endtime)) - int(time_to_seconds(ch.stats.starttime))
+                log_data(stat=station_name,data=f"from {ch.stats.starttime} to {ch.stats.endtime}")
+                log_data(stat=station_name,data=f"Time interval of the collected data : {time_interval} seconds")
+                log_data(stat=station_name,data=f'Waves Produced lenght : <{len(ch.data)}>')
+
+            JSON_value = json.dumps(value)
+            producer.produce(kafka_topic, key=station_name, value=JSON_value,callback=acked)
+            log_data(stat=station_name,data=f'Waves Produced succesfully')
+
+        except Exception as e:
+            print(f"Error for station {station_name}: {str(e)}")
+            time.sleep(2)
+
+
+station_names = ["JAGI", "BBJI","SMRI"]
+if __name__ == "__main__":
+    with multiprocessing.Pool(processes=len(station_names)) as pool:
+        pool.map(fetch_data, station_names)
---
+++ producer_pararel_test.py
@@ -0,0 +1,112 @@
+from confluent_kafka import Producer, KafkaError, KafkaException
+import certifi
+from obspy.clients.fdsn import Client
+from obspy import UTCDateTime
+import time
+from datetime import datetime
+from obspy.clients.seedlink import Client as SeedlinkClient
+from obspy.clients.seedlink.easyseedlink import create_client
+import pytz
+import json
+import configparser
+import threading
+import signal
+import sys
+
+# fungsi callback error
+def error_cb(err):
+    print("Client error: {}".format(err))
+    if err.code() == KafkaError._ALL_BROKERS_DOWN or \
+       err.code() == KafkaError._AUTHENTICATION:
+        raise KafkaException(err)
+    
+# Create a ConfigParser instance
+config = configparser.ConfigParser()
+# Load the configuration from the file
+config.read('kafka_config.txt')
+
+bootstrap_servers = config.get('KafkaConfig', 'bootstrap.servers')
+sasl_username = config.get('KafkaConfig', 'sasl.username')
+sasl_password = config.get('KafkaConfig', 'sasl.password')
+    
+# config untuk masuk ke akun confluent 
+kafka_config = {
+    'bootstrap.servers': bootstrap_servers, 
+    'sasl.mechanism': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': sasl_username,
+    'sasl.password': sasl_password,
+    "ssl.ca.location": certifi.where(),
+    'linger.ms': 100,
+    'error_cb': error_cb, 
+}
+
+# nama topic pada confluent
+kafka_topic = 'WAVE_Station' 
+# menggunakan Producer dengan memasukkan kafka_config
+producer = Producer(kafka_config)
+# seedlink untuk mengambil data dari stasiun sensor dengan realtime
+client = SeedlinkClient("geofon.gfz-potsdam.de", 18000)
+
+# fungsi untuk menampilkan log
+def log_data(data, type="LOG"):
+    print(f"[+] <{type}> {data}")
+
+
+# Mengambil data dengan SeedLink Client
+def get_data_seed_link(key_partition):
+    while True:
+        try:
+            # Mengambil waktu saat ini
+            utc_time = datetime.now(pytz.UTC)
+            formatted_time = utc_time.strftime('%Y-%m-%dT%H:%M:%S')
+            starttime = UTCDateTime(formatted_time) 
+            endtime = starttime
+            print("Producing data...")
+            st = client.get_waveforms("GE", key_partition, "*", "BH*", starttime - 60, endtime)
+            
+            # Extract and log the data
+            value = {}
+            log_data(f"Data from {key_partition} station")
+            for ch in st:
+                value[ch.stats.channel] = ch.data.tolist()
+                log_data(f"Data from {ch.stats.channel} fetch successfully !!")
+                log_data(f'Waves Produced length: <{len(ch.data)}>')
+
+            # Mengubah file dalam bentuk JSON
+            JSON_value = json.dumps(value)
+            
+            # Produce the JSON data to Kafka for the channel
+            producer.produce(kafka_topic, key=key_partition.encode('utf-8'), value=JSON_value.encode('utf-8'))
+            producer.flush()
+
+            print("\n")
+
+        except Exception as e:
+            log_data(f"Error fetching or producing data for key_partition {key_partition}: {str(e)}")
+
+station_java = ['JAGI', 'SMRI'] 
+
+# Create threads to fetch and produce data for each key_partition
+threads = []
+exit_signal = threading.Event()
+for station in station_java:
+    thread = threading.Thread(target=get_data_seed_link, args=(station,))
+    threads.append(thread)
+    thread.start()
+
+# Function to gracefully stop the threads and close the producer
+def stop_threads(signal, frame):
+    print("Stopping threads...")
+    exit_signal.set()  # Set the exit signal to stop the threads
+
+# Set the signal handler for SIGINT (Ctrl+C)
+signal.signal(signal.SIGINT, stop_threads)
+
+# Wait for threads to finish
+for thread in threads:
+    thread.join()
+
+# Close the Kafka producer
+producer.close()
+
